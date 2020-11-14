abe0ade348b6cbe0e052f7a78461c2a1acd3d0b7
leouemura <leo.uemura.s3@gmail.com>
2020-11-14 18:08:24 -0300

    master

--- src/controllers/NewController.js
+++ src/controllers/NewController.js
@@ -4,12 +4,12 @@ const crypto = require('crypto');
 
 module.exports = {
     async create(req,res){
-        let temperatura = Number((Math.random()+17).toFixed(2))
-        let umidade = Number((Math.random()*3 - Math.random()/10 + 70).toFixed(2))
+        let temperatura = (Math.random()+17).toFixed(2)
+        let umidade = (Math.random()*3 - Math.random()/10 + 70).toFixed(2)
         let porta1 = "True"
         let porta2 = "True"
         let porta3 = "True"
-        let temperatura2 = Number((Number(temperatura)+Math.random()/10).toFixed(2))
+        let temperatura2 = (Number(temperatura)+Math.random()/10).toFixed(2)
        // let umidade2 = (umidade + Math.random()/10).toString().toFixed(2)
         let id = crypto.randomBytes(4).toString('hex')
         
@@ -20,14 +20,14 @@ module.exports = {
     async index(req,res){
         const data = await connection('db_tablename').select('temperatura','umidade','porta1','porta2','temperatura2','porta3').limit(1).orderBy('id','desc')
         return res.json(
-            {data:{
+            {
                 temperatura:data[0].temperatura,
                 umidade:data[0].umidade,
                 porta1:data[0].porta1,
                 porta2:data[0].porta2,
                 temperatura2:data[0].temperatura2,
                 porta3:data[0].porta3
-            }}
+            }
         )
     },
 
--- src/database/migrations/00_db_tablename.js
+++ src/database/migrations/00_db_tablename.js
@@ -3,11 +3,11 @@
 exports.up = function(knex){
     return knex.schema.createTable('db_tablename', function(table){
         table.string('id').primary();
-        table.float('temperatura').notNullable();
-        table.float('umidade').notNullable();
+        table.string('temperatura').notNullable();
+        table.string('umidade').notNullable();
         table.string('porta1').notNullable();
         table.string('porta2').notNullable();
-        table.float('temperatura2').notNullable();
+        table.string('temperatura2').notNullable();
         table.string('porta3').notNullable();
     })
 }
