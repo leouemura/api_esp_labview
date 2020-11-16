4a1e082a22243bbd02be8ffff955f020bfa779c2
leouemura <leo.uemura.s3@gmail.com>
2020-11-16 14:20:45 -0300

    V.0.1

--- src/controllers/NewController.js
+++ src/controllers/NewController.js
@@ -4,14 +4,23 @@ const crypto = require('crypto');
 
 module.exports = {
     async create(req,res){
+        /*
         let temperatura = (Math.random()+17).toFixed(2)
         let umidade = (Math.random()*3 - Math.random()/10 + 70).toFixed(2)
         let porta1 = "True"
         let porta2 = "True"
-        let porta3 = "True"
+        let porta3 = "False"
         let temperatura2 = (Number(temperatura)+Math.random()/10).toFixed(2)
        // let umidade2 = (umidade + Math.random()/10).toString().toFixed(2)
-        
+        */
+       const {
+           temperatura,
+           umidade,
+           porta1,
+           porta2,
+           temperatura2,
+           porta3
+       } = req.body
         await connection('db_tablename').insert({temperatura,umidade,porta1,porta2,temperatura2,porta3});
         return res.json({temperatura,umidade,porta1,porta2,temperatura2,porta3})
     },
--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ const app = express()
 app.use(express.json());
 app.use(routes);
 
+
 app.listen(3333);
